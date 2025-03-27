976b9f37befa55a31631b6ac925b44b81f1cce2a
Teddy <httpdevz@gmail.com>
2025-03-27 18:11:00 -0400

    idk bro

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,7 @@
     let { children } = $props();
 </script>
 
-<main class="h-screen w-screen flex flex-col rounded-lg overflow-hidden">
+<main class="h-screen w-screen flex flex-col rounded-lg overflow-hidden border border-neutral-300 shadow-md">
     <TitleBar />
     <div class="h-full w-full bg-gray-50">
         {@render children()}
